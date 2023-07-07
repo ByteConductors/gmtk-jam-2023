6abdc5d2bef94d2dca849cd66580ca1ed5a289ac
mabe.edu <Max.Becker@student.hs-rm.de>
2023-07-07 21:02:12 +0200

    First Building Tiles
    added Tilemap and first Tiles

--- Assets/Scripts/MonoBehaviour/World/BuildingTile.cs
+++ Assets/Scripts/MonoBehaviour/World/BuildingTile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(menuName = "BuildingTile")]
-public class BuildingTile : TileBase
+public class BuildingTile : Tile
 {
     public string BuildingName;
     public string BuildingType;
