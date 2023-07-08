0866747186ab61dd76d2e7c42028d284c30d1035
marcmatija <marcmatija.mm@gmail.com>
2023-07-08 17:13:22 +0200

    Added Position to buildingTile

--- Assets/Scripts/MonoBehaviour/World/BuildingTile.cs
+++ Assets/Scripts/MonoBehaviour/World/BuildingTile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,9 +12,12 @@ public class BuildingTile : Tile
     public BuildingResource resource;
     public float deliveryTime;
     public Vector2Int size;
+    Vector3Int position;
+    public Vector3Int Position { get => position; set => position = value; }
+
+
 
     private void Awake()
     {
-           
     }
 }
