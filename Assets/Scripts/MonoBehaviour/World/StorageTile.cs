6ee71279d5e855443ed6497888ca478f7182fc5d
mabe.edu <Max.Becker@student.hs-rm.de>
2023-07-08 18:17:19 +0200

    ContextBoubbles added

--- Assets/Scripts/MonoBehaviour/World/StorageTile.cs
+++ Assets/Scripts/MonoBehaviour/World/StorageTile.cs
@@ -7,19 +7,23 @@ using UnityEngine.Tilemaps;
 public class StorageTile : BuildingTile,ICollisionTile 
 {
     [SerializeField]
-    public BuildingResource storedResource;
+    private BuildingResource storedResource;
 
-    public void setStoredResource(BuildingResource resource)
+    public void onPlace()
     {
-        storedResource = resource;
+        if (Position == null || Position == new Vector3Int(0,0,0)) return;
+        Debug.Log("Storage Position: " + Position);
+        ContextBoubbleManager.instance.showContextBoubble(Position, storedResource.Icon);
     }
 
     public void OnCollision() //change ressource that the player is carrying
     {
         if (PlayerController.instance == null) return;
 
+        onPlace();
+
         // if player is already carrying a resource, do nothing
-        if (PlayerController.instance.getResource() != null) return;
+        //if (PlayerController.instance.getResource() != null) return;
         PlayerController.instance.setResource(storedResource);
         Debug.Log("Player is now carrying " + storedResource.name);
     }
