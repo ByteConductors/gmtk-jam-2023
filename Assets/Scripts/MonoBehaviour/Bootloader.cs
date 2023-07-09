dcbec5a0eab7cbdabee168e64e94604ce1a3cd1d
marcmatija <marcmatija.mm@gmail.com>
2023-07-09 03:22:06 +0200

    Added Bootloader

--- Assets/Scripts/MonoBehaviour/Bootloader.cs
+++ Assets/Scripts/MonoBehaviour/Bootloader.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bootloader : MonoBehaviour
 {
     public string UIScene;
     public string bootScene;
 
+    public GameObject gameMangerPrefab;
     public GameObject playerPrefab;
+    public GameObject pointerPrefab;
+    public GameObject LevelPrefab;
 
+    private void Awake()
+    {
+        StartCoroutine(Boot());
+    }
+
+    IEnumerator Boot()
+    {
+        SceneManager.LoadSceneAsync(UIScene, LoadSceneMode.Additive);
+
+        yield return null;
+    }
 }
